d13a678930d1acc039eb3aa5c3f711bfed4e6eca
snaulX <alexandergunger666@gmail.com>
2019-12-01 12:05:14 +0300

    Create Unity project and small part of main menu

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField]
+    public Texture2D cursor_icon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursor_icon, new Vector2(), CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
