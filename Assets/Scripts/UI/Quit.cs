f65172bfb4b42a9b96857c70d4d3c840fc75db4a
snaulX <alexandergunger666@gmail.com>
2019-12-02 10:21:10 +0300

    Fix UI

---
+++ Assets/Scripts/UI/Quit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        objHandler.AddReference("Quit", gameObject);
+        gameObject.GetComponent<Button>().onClick.AddListener(() => Application.Quit());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
