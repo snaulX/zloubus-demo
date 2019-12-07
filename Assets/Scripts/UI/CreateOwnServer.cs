994b4f41a660a51ad6f1c6ead701e1a58f75cb5a
snaulX <alexandergunger666@gmail.com>
2019-12-07 08:37:54 +0300

    Create NetworkMaster

--- Assets/Scripts/UI/CreateOwnServer.cs
+++ Assets/Scripts/UI/CreateOwnServer.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿#pragma warning disable CS0618 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
 
 public class CreateOwnServer : MonoBehaviour
 {
@@ -8,6 +12,7 @@ public class CreateOwnServer : MonoBehaviour
     void Start()
     {
         objHandler.AddReference("CreateOwnServer", gameObject);
+        GetComponent<Button>().onClick.AddListener(OnClick);
         gameObject.SetActive(false);
     }
 
@@ -15,5 +20,11 @@ public class CreateOwnServer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnClick()
+    {
+        NetworkMaster nm = GameObject.Find("NetworkMaster").GetComponent<NetworkMaster>();
+        nm.StartServer();
     }
 }
